6f64a3d328eea593e48a5928c6cbd399b6925125
heimdall-exe <francesco.pierno000@gmail.com>
2025-06-24 17:50:48 +0200

    Add files via upload

---
+++ Martedì - Prima settimana/ProvaVariabili.java
@@ -0,0 +1,7 @@
+public class ProvaVariabili {
+    public static void main(String[] args) {
+        int x=5, y=10, z=15;
+        System.out.println(x+y-z);
+
+    }
+}
---
+++ Martedì - Prima settimana/UseArgument.java
@@ -0,0 +1,9 @@
+public class UseArgument {
+
+    public static void main(String[] args) {
+        String pippo = "pippo";
+        System.out.println("Hi, ");
+        System.out.println(pippo);
+        System.out.println(". How are you?");
+        }
+}
