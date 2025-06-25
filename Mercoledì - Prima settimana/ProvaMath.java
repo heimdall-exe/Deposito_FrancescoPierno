0dbbea1b2a0989966528b72cf4475f1cfd938569
heimdall-exe <francesco.pierno000@gmail.com>
2025-06-25 15:32:27 +0200

    Add files via upload

---
+++ Mercoledì - Prima settimana/ProvaMath.java
@@ -0,0 +1,9 @@
+public class ProvaMath {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        Math.min(x, y);
+        Math.max(x, y);
+        Math.sqrt(x);
+    }
+}
